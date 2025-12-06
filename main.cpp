006accc61f1e8b4195aa7a79b46797fae4b6f118
KristevT <timkristev.cloud@gmail.com>
2025-12-06 16:32:32 +0300

    menu bar placeholder, fixed tool picking

--- main.cpp
+++ main.cpp
@@ -73,6 +73,18 @@ struct ToolButton {
     Texture2D icon;
 };
 
+struct MenuItem {
+    std::string label;
+    Rectangle rect;
+};
+
+struct MenuTab {
+    std::string label;
+    Rectangle rect;
+    std::vector<MenuItem> items;
+    bool open = false;
+};
+
 int main() {
     const int screenWidth = 1200;
     const int screenHeight = 800;
@@ -80,11 +92,15 @@ int main() {
     raylib::Window window(screenWidth, screenHeight, "ratart - Simple Drawing App");
     SetTargetFPS(60);
 
-    const int menuBarHeight = 30;
+    const int menuBarHeight = 25;
     const int toolbarWidth = 150;
 
     // Current tool (Pencil by default)
-    std::unique_ptr<Tool> currentTool = std::make_unique<PencilTool>();
+    std::unique_ptr<PencilTool> pencilTool = std::make_unique<PencilTool>();
+    // std::unique_ptr<EraserTool> eraserTool = std::make_unique<EraserTool>();
+    // ...
+
+    Tool* currentTool = pencilTool.get();
 
     std::vector<ToolButton> toolButtons = {
         { "Pencil",  "icons/pencil.png",  {}, KEY_P, ToolButton::PENCIL },
@@ -98,6 +114,22 @@ int main() {
     // Load icons
     for (auto& b : toolButtons)
         b.icon = LoadTexture(b.iconPath.c_str());
+
+    std::vector<MenuTab> menu = {
+        { "File",   { 0, 0, menuBarHeight * 2, menuBarHeight },
+            { {"New", {}}, {"Open", {}}, {"Save", {}}, {"Save As", {}} }
+        },
+        { "Edit",   { menuBarHeight * 2, 0, menuBarHeight * 2, menuBarHeight },
+            { {"Copy", {}}, {"Cut", {}}, {"Paste", {}} }
+        },
+        { "Canvas", { menuBarHeight * 4, 0, menuBarHeight * 3.5, menuBarHeight },
+            { {"Change Canvas Size", {}}, {"Change Canvas BG", {}} }
+        }
+    };
+
+    Rectangle undoBtn = { menuBarHeight * 7.5, 0, menuBarHeight * 2.5, menuBarHeight };
+    Rectangle redoBtn = { menuBarHeight * 10, 0, menuBarHeight * 2.5, menuBarHeight };
+
 
     while (!window.ShouldClose()) {
         // --- Update logic here (placeholder) ---
@@ -120,12 +152,12 @@ int main() {
         for (auto& b : toolButtons) {
             if (IsKeyPressed(b.shortcut)) {
                 switch (b.id) {
-                    case ToolButton::PENCIL: currentTool = std::make_unique<PencilTool>(); break;
-                    case ToolButton::ERASER: /* create EraserTool */ break;
-                    case ToolButton::DROPPER: /* create DropperTool */ break;
-                    case ToolButton::BUCKET: /* create BucketTool */ break;
-                    case ToolButton::SQUARE: /* create RectangleTool */ break;
-                    case ToolButton::CIRCLE: /* create CircleTool */ break;
+                    case ToolButton::PENCIL: currentTool = pencilTool.get(); break;
+                    case ToolButton::ERASER: /* currentTool = eraserTool.get(); */ break;
+                    case ToolButton::DROPPER: /* currentTool = dropperTool.get(); */ break;
+                    case ToolButton::BUCKET: /* currentTool = bucketTool.get(); */ break;
+                    case ToolButton::SQUARE: /* currentTool = squareTool.get(); */ break;
+                    case ToolButton::CIRCLE: /* currentTool = circleTool.get(); */ break;
                 }
             }
         }
@@ -138,10 +170,6 @@ int main() {
         DrawRectangleLines(toolbarWidth, menuBarHeight, screenWidth - toolbarWidth, screenHeight - menuBarHeight, LIGHTGRAY);
 
         currentTool->Draw();
-
-        // MENU BAR
-        DrawRectangle(0, 0, screenWidth, menuBarHeight, LIGHTGRAY);
-        DrawText("File  Edit  Canvas  Undo  Redo", 10, 7, 16, BLACK);
 
         // TOOL BAR
         DrawRectangle(0, menuBarHeight, toolbarWidth, screenHeight - menuBarHeight, ColorFromHSV(0, 0, 25));
@@ -211,18 +239,101 @@ int main() {
             // Click handler
             if (CheckCollisionPointRec(mouse, b.rect) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
                 switch (b.id) {
-                    case ToolButton::PENCIL: currentTool = std::make_unique<PencilTool>(); break;
-                    case ToolButton::ERASER: /* create EraserTool */ break;
-                    case ToolButton::DROPPER: /* create DropperTool */ break;
-                    case ToolButton::BUCKET: /* create BucketTool */ break;
-                    case ToolButton::SQUARE: /* create RectangleTool */ break;
-                    case ToolButton::CIRCLE: /* create CircleTool */ break;
+                    case ToolButton::PENCIL: currentTool = pencilTool.get(); break;
+                    case ToolButton::ERASER: /* currentTool = eraserTool.get(); */ break;
+                    case ToolButton::DROPPER: /* currentTool = dropperTool.get(); */ break;
+                    case ToolButton::BUCKET: /* currentTool = bucketTool.get(); */ break;
+                    case ToolButton::SQUARE: /* currentTool = squareTool.get(); */ break;
+                    case ToolButton::CIRCLE: /* currentTool = circleTool.get(); */ break;
                 }
             }
         }
 
         // Size slider UI for current tool
         currentTool->DrawUI((toolbarWidth - 100) * 0.5, toolSectionHeight + iconSize * 2 + 60);
+
+        // MENU BAR
+        DrawRectangle(0, 0, screenWidth, menuBarHeight, LIGHTGRAY);
+
+        Vector2 m = GetMousePosition();
+
+        // Draw tabs & handle clicks
+        for (auto& tab : menu) {
+            Color bg = (tab.open || CheckCollisionPointRec(m, tab.rect)) ?
+                    GRAY :
+                    LIGHTGRAY;
+
+            DrawRectangleRec(tab.rect, bg);
+            DrawText(tab.label.c_str(), tab.rect.x + menuBarHeight * 0.3, tab.rect.y + menuBarHeight * 0.1, menuBarHeight * 0.8, BLACK);
+
+            // Toggle dropdown
+            if (CheckCollisionPointRec(m, tab.rect) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                for (auto& t : menu) t.open = false;
+                tab.open = true;
+            }
+
+            // Draw dropdown
+            if (tab.open) {
+                int itemHeight = 22;
+                int menuWidth = 180;
+
+                for (int i = 0; i < tab.items.size(); i++) {
+                    MenuItem& it = tab.items[i];
+                    it.rect = {
+                        tab.rect.x,
+                        tab.rect.y + tab.rect.height + i * itemHeight,
+                        (float)menuWidth,
+                        (float)itemHeight
+                    };
+
+                    Color bg2 = CheckCollisionPointRec(m, it.rect)
+                                ? Color{220, 220, 220, 255}
+                                : Color{245, 245, 245, 255};
+
+                    DrawRectangleRec(it.rect, bg2);
+                    DrawRectangleLinesEx(it.rect, 1, BLACK);
+                    DrawText(it.label.c_str(), it.rect.x + 6, it.rect.y + 4, 16, BLACK);
+
+                    // Placeholder functionality
+                    if (CheckCollisionPointRec(m, it.rect) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                        printf("Menu clicked: %s -> %s\n", tab.label.c_str(), it.label.c_str());
+                        tab.open = false;
+                    }
+                }
+            }
+        }
+
+        // Close all menus if clicked outside
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            bool clickedMenu = false;
+
+            for (auto& tab : menu) {
+                if (CheckCollisionPointRec(m, tab.rect)) clickedMenu = true;
+                for (auto& it : tab.items)
+                    if (CheckCollisionPointRec(m, it.rect)) clickedMenu = true;
+            }
+
+            if (!clickedMenu)
+                for (auto& tab : menu) tab.open = false;
+        }
+
+        // Undo / Redo
+        DrawRectangleRec(undoBtn, CheckCollisionPointRec(m, undoBtn) ? GRAY : LIGHTGRAY);
+        DrawRectangleRec(redoBtn, CheckCollisionPointRec(m, redoBtn) ? GRAY : LIGHTGRAY);
+
+        DrawText("Undo", undoBtn.x + menuBarHeight * 0.3, undoBtn.y + menuBarHeight * 0.1, menuBarHeight * 0.8, BLACK);
+        DrawText("Redo", redoBtn.x + menuBarHeight * 0.3, redoBtn.y + menuBarHeight * 0.1 , menuBarHeight * 0.8, BLACK);
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            if (CheckCollisionPointRec(m, undoBtn)) {
+                printf("Undo clicked\n");
+                // TODO: Undo logic
+            }
+            if (CheckCollisionPointRec(m, redoBtn)) {
+                printf("Redo clicked\n");
+                // TODO: Redo logic
+            }
+        }
 
         EndDrawing();
     }
